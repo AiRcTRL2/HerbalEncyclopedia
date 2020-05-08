613a11be9aa9cc04e236a9c09feae7e9e84c2c7f
Karim El Gendy <karim.elgendy@ucdconnect.ie>
2020-05-08 12:39:29 +0100

    Initial Commit

---
+++ Herbal Encyclopedia/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Herbal Encyclopedia
+//
+//  Created by Karim Elgendy on 08/05/2020.
+//  Copyright © 2020 Rebellion Media. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

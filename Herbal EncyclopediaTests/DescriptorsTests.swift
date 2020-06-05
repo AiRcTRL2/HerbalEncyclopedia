921218737061ac5938ee006c8ea14a209fa959b8
Karim El Gendy <karim.elgendy@ucdconnect.ie>
2020-06-05 23:04:11 +0100

    Add test class for DescriptorViewModel

---
+++ Herbal EncyclopediaTests/DescriptorsTests.swift
@@ -0,0 +1,38 @@
+//
+//  DescriptorsTests.swift
+//  Herbal EncyclopediaTests
+//
+//  Created by Karim Elgendy on 05/06/2020.
+//  Copyright © 2020 Rebellion Media. All rights reserved.
+//
+
+import XCTest
+
+@testable import Herbal_Encyclopedia
+class DescriptorsTests: XCTestCase {
+    var descriptorModel: DescriptorViewModel!
+    
+    override func setUpWithError() throws {
+        super.setUp()
+        descriptorModel = DescriptorViewModel(pageTitle: "test", jsonFileIdentifier: "body_effects", descriptorTitles: ["Antipasmodic"], descriptorExplanations: nil)
+    }
+
+    override func tearDownWithError() throws {
+        super.tearDown()
+        descriptorModel = nil
+    }
+
+    func testExample() throws {
+        descriptorModel.parseExplanationsFromTitles()
+        print(descriptorModel.descriptorExplanations)
+        XCTAssert(!descriptorModel.descriptorExplanations!.isEmpty)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

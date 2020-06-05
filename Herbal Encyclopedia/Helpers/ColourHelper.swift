5f964b5e1224c3b4ae2e04cde5314d51ab5806cd
Karim El Gendy <karim.elgendy@ucdconnect.ie>
2020-06-05 23:05:39 +0100

    Add colour helper class

---
+++ Herbal Encyclopedia/Helpers/ColourHelper.swift
@@ -0,0 +1,16 @@
+//
+//  ColourHelper.swift
+//  Herbal Encyclopedia
+//
+//  Created by Karim Elgendy on 28/05/2020.
+//  Copyright © 2020 Rebellion Media. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct ColourHelper {
+    static func separatorGrey(alpha: CGFloat = 1.0) -> UIColor {
+        UIColor(red: 220 / 255.0, green: 220 / 255.0, blue: 220 / 255.0, alpha: alpha)
+    }
+}

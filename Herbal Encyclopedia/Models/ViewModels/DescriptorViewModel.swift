235487736fccc52f9e38b5d8e68460d06a928f55
Karim El Gendy <karim.elgendy@ucdconnect.ie>
2020-06-18 21:27:09 +0100

    Add Enum  to check expected page titles

--- Herbal Encyclopedia/Models/ViewModels/DescriptorViewModel.swift
+++ Herbal Encyclopedia/Models/ViewModels/DescriptorViewModel.swift
@@ -8,13 +8,18 @@
 
 import Foundation
 
+private enum PlantSpecficExplanations {
+    static let bodilyEffects = "Bodily effects"
+    static let preparation = "Preparation"
+}
+
 struct DescriptorViewModel {
     var pageTitle: String
-    /**
-     The json file to open with the desired descriptions.
-     */
+    /// The json file to open with the desired descriptions.
     var jsonFileIdentifier: String?
     var descriptorTitles: [String]
+    
+    // sets the size of the tableview data source once explanations have been parsed
     var descriptorExplanations: [String]? {
         didSet {
             if descriptorTitles.count == 1 {
@@ -27,11 +32,13 @@ struct DescriptorViewModel {
         }
     }
     var describingPlant: Plant?
-    /**
-     This list is responsible for the amalgamation of both the titles and explanations lists in the most efficient manner known.
-     */
+
+    /// This list is responsible for the amalgamation of both the titles and explanations lists in the most efficient manner known.
     var tableViewDataSource: [String]?
     
+    /**
+     Attempts to parse explanations from the json document reflecting the requested explanation titles. Will also attempt to parse plant specific explanations for the same title, amalgamating these two values in separate paragraphs.
+     */
     mutating func parseExplanationsFromTitles() {
         var dict: [String: String]?
         
@@ -55,15 +62,41 @@ struct DescriptorViewModel {
         // check if dict values are available
         if let dictUnwrapped = dict {
             var tempDict: [String] = []
+            // get plant specific additions for this page title
+            let plantSpecificAdditions: [String:String]? = parsePlantSpecificExplanationForPageTitle(pageTitle: self.pageTitle)
             // parse explanations for titles required by the view controller (descriptions requested)
             for title in descriptorTitles {
-                if let explanation = dictUnwrapped[title] {
+                if var explanation = dictUnwrapped[title] {
+                    // check if the plant also has a plant specific explanation (rather than only generic) for this page title
+                    if let plantSpecificExplanation = plantSpecificAdditions?[title]  {
+                        explanation = "\(explanation) \n\n\(plantSpecificExplanation)"
+                    }
                     tempDict.append(explanation)
                 }
             }
 
             // set the struct's explanations for use by the view
             self.descriptorExplanations = tempDict
+        }
+    }
+    
+    /**
+     This function attempts to fetch the plant specific explanation dictionary for the required plant section. It determines the correct plant section by maging the set view controller's title against the PlantSpecificExplanations enum.
+     
+     - Parameters:
+        - pageTitle: String representation of the view controller's set page title
+     
+     - Returns:
+        - A dictionary of title: explanation for the plant section that has a plant specific explanation, if one is found.
+     */
+    func parsePlantSpecificExplanationForPageTitle(pageTitle: String) -> [String:String]? {
+        switch pageTitle {
+        case PlantSpecficExplanations.bodilyEffects:
+            return describingPlant?.bodilyEffects.plantSpecific
+        case PlantSpecficExplanations.preparation:
+            return describingPlant?.preparations.plantSpecific
+        default:
+            return nil
         }
     }
 }

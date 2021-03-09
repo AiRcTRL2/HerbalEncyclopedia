996d4abef5d963ab0bc09ef42bceb492dc88e101
Karim El Gendy <karim.elgendy@ucdconnect.ie>
2021-03-09 13:59:04 +0000

    Add model for parsing plant specific dictionary

---
+++ Herbal Encyclopedia/Data Models/PlantDescipritonContainer.swift
@@ -0,0 +1,13 @@
+//
+//  PlantDescipritonContainer.swift
+//  Herbal Encyclopedia
+//
+//  Created by Karim Elgendy on 08/03/2021.
+//  Copyright © 2021 Rebellion Media. All rights reserved.
+//
+
+import Foundation
+
+struct PlantDescriptionContainer: Codable {
+    var data: [String: String]
+}

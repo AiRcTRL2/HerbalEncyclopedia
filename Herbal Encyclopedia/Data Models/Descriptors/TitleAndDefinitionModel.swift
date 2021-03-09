f907dcf41199c20277e0f6dad9015a5bfeb923ad
Karim El Gendy <karim.elgendy@ucdconnect.ie>
2021-03-09 13:54:27 +0000

    Add data model / source for defining titles

---
+++ Herbal Encyclopedia/Data Models/Descriptors/TitleAndDefinitionModel.swift
@@ -0,0 +1,15 @@
+//
+//  PlantDescriptorModel.swift
+//  Herbal Encyclopedia
+//
+//  Created by Karim Elgendy on 08/03/2021.
+//  Copyright © 2021 Rebellion Media. All rights reserved.
+//
+
+import Foundation
+
+/// This data model serves as a data source for presenting Terms along with their definition/s for a tableView.
+struct TitleAndDefinitionModel {
+    let title: String
+    let description: String
+}

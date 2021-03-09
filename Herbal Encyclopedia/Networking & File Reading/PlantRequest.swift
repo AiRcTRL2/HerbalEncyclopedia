68e9d397a6fb60f0d3e297ff3cfae2ca4a4de979
Karim El Gendy <karim.elgendy@ucdconnect.ie>
2021-03-09 22:20:48 +0000

    Add logic for PlantRequest

--- Herbal Encyclopedia/Networking & File Reading/PlantRequest.swift
+++ Herbal Encyclopedia/Networking & File Reading/PlantRequest.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+protocol PlantRequestProtocol {
+    func fetchPlants() -> [Plant]?
+}
+
+struct PlantRequest: PlantRequestProtocol {
+    let request = Request()
+    
+    func fetchPlants() -> [Plant]? {
+        request.configure(requestType: .file, urlOrFileName: "herbs")
+        
+        guard let plants: [Plant]? = request.request() else {
+            print("An error occurred when attempting to fetch resource")
+            return nil
+        }
+        
+        return plants
+    }
+    
+    
+}

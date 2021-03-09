405e851bfdfa109d7ae2e96ecdb33fa8812c0ceb
Karim El Gendy <karim.elgendy@ucdconnect.ie>
2021-03-09 13:55:37 +0000

    Add generic dictionary container for parsing JSON

---
+++ Herbal Encyclopedia/Data Models/Descriptors/DictionaryContainer.swift
@@ -0,0 +1,13 @@
+//
+//  DictionaryContainer.swift
+//  Herbal Encyclopedia
+//
+//  Created by Karim Elgendy on 08/03/2021.
+//  Copyright © 2021 Rebellion Media. All rights reserved.
+//
+
+import Foundation
+
+struct DictionaryContainer: Codable {
+    let data: [String: String]
+}

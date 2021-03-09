a007677f5d89868fc9dcb609fa89ad0281c52b69
Karim El Gendy <karim.elgendy@ucdconnect.ie>
2021-03-09 22:19:36 +0000

    Add PlantRequest Class

---
+++ Herbal Encyclopedia/Networking & File Reading/PlantRequest.swift
@@ -0,0 +1,9 @@
+//
+//  PlantRequest.swift
+//  Herbal Encyclopedia
+//
+//  Created by Karim Elgendy on 09/03/2021.
+//  Copyright © 2021 Rebellion Media. All rights reserved.
+//
+
+import Foundation

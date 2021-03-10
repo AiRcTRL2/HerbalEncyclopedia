c7e349dde0195d8f3ac068a50300dcfe0faa618a
Karim El Gendy <karim.elgendy@ucdconnect.ie>
2021-03-10 16:52:48 +0000

    Delete silly code

--- Herbal Encyclopedia/Data Models/Recipes/RecipesCategoriesContainerModel.swift
+++ Herbal Encyclopedia/Data Models/Recipes/RecipesCategoriesContainerModel.swift
@@ -10,32 +10,4 @@ import Foundation
 
 struct RecipesCategoriesContainerModel: Codable {
     let data: [RecipeCategoryModel]
-    
-    /// Returns an object of type RecipesCategoriesContainerModel, containing recipe categories which contains recipe models.
-    static func readJson() -> RecipesCategoriesContainerModel? {
-        if let path = Bundle.main.url(forResource: "recipes", withExtension: "json") {
-            do {
-                let data = try Data(contentsOf: path, options: .mappedIfSafe)
-                return self.decodeData(data: data)
-            } catch {
-                print(error)
-            }
-        }
-        
-        return nil
-    }
-    
-    /// Use readJson (decodeData only used externally for testing purposes)
-    static func decodeData(data: Data) -> RecipesCategoriesContainerModel? {
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        do {
-            let recipesCategoryObject = try decoder.decode(RecipesCategoriesContainerModel.self, from: data)
-            return recipesCategoryObject
-        } catch {
-            print(error)
-        }
-        
-        return nil
-    }
 }

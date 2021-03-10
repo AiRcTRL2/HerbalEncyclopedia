7522b8496a6cc6a9bc4161ecc91d1a145244ef23
Karim El Gendy <karim.elgendy@ucdconnect.ie>
2021-03-10 14:52:37 +0000

    Remove code not appropriate for location

--- Herbal Encyclopedia/UI Extensions/UIViewExtensions.swift
+++ Herbal Encyclopedia/UI Extensions/UIViewExtensions.swift
@@ -53,29 +53,4 @@ extension UIView {
 
         layer.addSublayer(border)
     }
-    
-    func bulletPointAttributedString(stringList: [String]) -> NSMutableAttributedString {
-        let style = NSMutableParagraphStyle()
-        style.alignment = .left
-        style.headIndent = 13
-        style.lineSpacing = 3
-
-        let mainString = NSMutableAttributedString()
-        
-        if stringList.count > 1 {
-            for string in stringList {
-                let bulletPoint = NSMutableAttributedString(string: "\u{2022}   ", attributes: [NSAttributedString.Key.paragraphStyle: style, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 10)])
-                bulletPoint.append(NSAttributedString(string: (string), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 17, weight: .light)]))
-                if string != stringList.last {
-                    bulletPoint.append(NSMutableAttributedString(string: "\n"))
-                }
-                mainString.append(bulletPoint)
-            }
-        } else if stringList.count == 1 {
-            mainString.append(NSAttributedString(string: stringList[0]))
-        }
-        
-    
-        return mainString
-    }
 }
